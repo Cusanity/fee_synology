cb1a9bbc3abfdcea4be9025e6adce843afb4e378
Yanchao Wang <wyc935398521@gmail.com>
2025-06-01 16:18:03 -0500

    php config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const {defineConfig} = require('@vue/cli-service')
+const fs = require('fs-extra')
+const path = require('path')
+
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: './',
@@ -11,4 +14,25 @@ module.exports = defineConfig({
     devServer: {
         port: 8010,
     },
+    // 添加构建完成后的钩子
+    configureWebpack: {
+        plugins: [
+            {
+                apply: (compiler) => {
+                    compiler.hooks.afterEmit.tap('CopyPHPFiles', (compilation) => {
+                        const srcDir = path.resolve(__dirname, 'src/php')
+                        const destDir = path.resolve(__dirname, 'docs/php')
+                        
+                        if (fs.existsSync(srcDir)) {
+                            console.log('Copying PHP files from src/php to docs/php...')
+                            fs.copySync(srcDir, destDir)
+                            console.log('PHP files copied successfully!')
+                        } else {
+                            console.log('src/php directory does not exist, skipping copy.')
+                        }
+                    })
+                }
+            }
+        ]
+    }
 })
